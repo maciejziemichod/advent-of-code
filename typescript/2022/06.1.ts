162a281545f956b0fd5c473dfd58247cdcc9f31d
maciejziemichod <maciej.ziemichod@gmail.com>
2023-11-30 00:37:34 +0100

    feat: ts/2022/06.1

---
+++ typescript/2022/06.1.ts
@@ -0,0 +1,21 @@
+import { runWithInput } from "../runWithInput";
+
+function main(input: string): void {
+	const chars = input.trim().split("");
+	console.log(findUniqueSequence(chars, 4));
+}
+
+function findUniqueSequence(array: any[], length: number): number {
+	for (let i = length; i <= array.length; i++) {
+		if (!hasDuplicates(array.slice(i - length, i))) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+function hasDuplicates(array: any[]): boolean {
+	return array.length !== new Set(array).size;
+}
+
+runWithInput(__dirname, "06.input", main);
